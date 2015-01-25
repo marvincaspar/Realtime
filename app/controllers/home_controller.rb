cc90e1a3a7e1165fd48b4d3e1d70ca240c28fcbf
mc388 <marvin.caspar@gmail.com>
2015-01-25 10:52:32 +0100

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,7 @@ class HomeController < ApplicationController
              notification: "<strong>user:</strong> #{ params[:message] }"
            }
            
-    $redis.publish("chat", data.to_json)
-    # $redis.publish("user-1:notification", data.to_json)
+    $redis.publish("user-1:notification", data.to_json)
 
     render nothing: true
   end
